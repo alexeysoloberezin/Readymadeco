5375a87dba9be744aae04fb56149e374bd40e873
alexey <alexeysoloberezinsolo@gmail.com>
2020-11-20 16:29:43 +0300

    zxc

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-// var bulls = document.querySelectorAll('.header__bull');
-
-// for (var bull of bulls){
-//   bull.addEventListener('click', function(){
-//     if (this.classList.contains('bull-active')){     
-//       this.classList.add('bull-active');
-//     } else{
-  
-//     }
-   
-   
-//   });
-// }
-
 var burger = document.querySelector('.burger'); 
 var menu = document.querySelector('.header__list'); 
 
@@ -20,3 +6,4 @@ burger.addEventListener('click', function(){
   menu.classList.toggle('header__list-active');
   burger.classList.toggle('active-burger');
 });
+
